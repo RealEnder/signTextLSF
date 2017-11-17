a47c1c1e08ad9ac65bb3c14ccd00218a9d61e729
Стамо Петков <sgpetkov@is-bg.net>
2017-11-17 17:44:08 +0200

    Edge specific files removed

--- Edge/contentscript.js
+++
@@ -1,6 +0,0 @@
-var s = document.createElement('script');
-s.src = browser.extension.getURL('script.js');
-s.addEventListener('load', function () {
-    this.parentNode.removeChild(this);
-}, false);
-(document.head || document.documentElement).appendChild(s);
